22c682be9121dcb4fe16bc0c2a9bbe978b0546c8
BestPig <bestpig82@gmail.com>
2015-02-01 03:04:41 +0100

    SystemUI: Add USB Tether tile
    Author: BestPig <bestpig82@gmail.com>
    Date:   Sun Feb 1 03:04:41 2015 +0100
    
        SystemUI: add USB Tether tile
    
         * Added vector drawables (blunden)
    
         * Long click action changed to open Tethering options, in the same way
           as Google does for Hotspot tile (bgcngm)
    
         * USB Tethering enablement check refactored (bgcngm and luca020400)
    
        Change-Id: Ic44f2b596fe32152f3f722347a855325c52113c6
    
    Author: Gabriele M <moto.falcon.git@gmail.com>
    Date:   Fri Apr 27 20:51:42 2018 +0200
    
        SystemUI: Toggle USB tethering only when USB is connected
    
        Pressing the tile when unplugged causes USB tethering to turn on
        as soon as the cable is connected. This can happen long after the
        tile has been pressed, so don't do anything if unplugged.
    
        Change-Id: Ib8bbdc758097ca7af5d4e7c3483c28ba42fb9ce1
    
    Change-Id: Ibbc0ba9de2cc3eee2a7f6a9c9f005730244f0b3a
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -57,6 +57,7 @@ import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
 import com.android.systemui.qs.tiles.SleepModeTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
+import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
 import com.android.systemui.util.leak.GarbageMonitor;
@@ -117,6 +118,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<SleepModeTile> mSleepModeTileProvider;
     private final Provider<CaffeineTile> mCaffeineTileProvider;
     private final Provider<AmbientDisplayTile> mAmbientDisplayTileProvider;
+    private final Provider<UsbTetherTile> mUsbTetherTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -157,7 +159,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<DreamTile> dreamTileProvider,
             Provider<SleepModeTile> sleepModeTileProvider,
             Provider<CaffeineTile> caffeineTileProvider,
-            Provider<AmbientDisplayTile> ambientDisplayTileProvider) {
+            Provider<AmbientDisplayTile> ambientDisplayTileProvider,
+            Provider<UsbTetherTile> usbTetherTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -194,6 +197,7 @@ public class QSFactoryImpl implements QSFactory {
         mSleepModeTileProvider = sleepModeTileProvider;
         mCaffeineTileProvider = caffeineTileProvider;
         mAmbientDisplayTileProvider = ambientDisplayTileProvider;
+        mUsbTetherTileProvider = usbTetherTileProvider;
     }
 
     /** Creates a tile with a type based on {@code tileSpec} */
@@ -275,6 +279,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mCaffeineTileProvider.get();
             case "ambient_display":
                 return mAmbientDisplayTileProvider.get();
+            case "usb_tether":
+                return mUsbTetherTileProvider.get();
         }
 
         // Custom tiles
