a6c1397e7c5e495488c39fe76c60b06103f0c5b5
Danny Baumann <dannybaumann@web.de>
2022-07-20 15:53:13 +0200

    SystemUI: Fix QS header clock color
    Now that we're flipping QS colors by theme (dark/light), we can no longer
    rely on wallpaper colors for QS clock. Instead, we now can rely on clock color
    being updated correctly on QS re-inflation (via
    QuickStatusBarHeader.updateResources).
    
    Change-Id: Icdf2484793cb63b7c0ab6ab87e94185e6bdc9ca4
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/tests/src/com/android/systemui/qs/QuickStatusBarHeaderControllerTest.kt
+++ packages/SystemUI/tests/src/com/android/systemui/qs/QuickStatusBarHeaderControllerTest.kt
@@ -20,6 +20,21 @@ import android.content.Context
 import android.testing.AndroidTestingRunner
 import androidx.test.filters.SmallTest
 import com.android.systemui.SysuiTestCase
+import com.android.systemui.battery.BatteryMeterViewController
+import com.android.systemui.demomode.DemoModeController
+import com.android.systemui.flags.FeatureFlags
+import com.android.systemui.qs.carrier.QSCarrierGroup
+import com.android.systemui.qs.carrier.QSCarrierGroupController
+import com.android.systemui.statusbar.phone.StatusBarContentInsetsProvider
+import com.android.systemui.statusbar.phone.StatusBarIconController
+import com.android.systemui.statusbar.phone.StatusIconContainer
+import com.android.systemui.statusbar.policy.Clock
+import com.android.systemui.statusbar.policy.VariableDateView
+import com.android.systemui.statusbar.policy.VariableDateViewController
+import com.android.systemui.util.mockito.any
+import com.android.systemui.util.mockito.argumentCaptor
+import com.android.systemui.util.mockito.capture
+import com.google.common.truth.Truth.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -38,7 +53,26 @@ class QuickStatusBarHeaderControllerTest : SysuiTestCase() {
     private lateinit var view: QuickStatusBarHeader
     @Mock
     private lateinit var quickQSPanelController: QuickQSPanelController
-
+    @Mock(answer = Answers.RETURNS_SELF)
+    private lateinit var qsCarrierGroupControllerBuilder: QSCarrierGroupController.Builder
+    @Mock
+    private lateinit var qsCarrierGroupController: QSCarrierGroupController
+    @Mock
+    private lateinit var iconContainer: StatusIconContainer
+    @Mock
+    private lateinit var qsCarrierGroup: QSCarrierGroup
+    @Mock
+    private lateinit var variableDateViewControllerFactory: VariableDateViewController.Factory
+    @Mock
+    private lateinit var variableDateViewController: VariableDateViewController
+    @Mock
+    private lateinit var batteryMeterViewController: BatteryMeterViewController
+    @Mock
+    private lateinit var clock: Clock
+    @Mock
+    private lateinit var variableDateView: VariableDateView
+    @Mock
+    private lateinit var mockView: View
     @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     private lateinit var context: Context
 
@@ -51,7 +85,20 @@ class QuickStatusBarHeaderControllerTest : SysuiTestCase() {
         `when`(view.isAttachedToWindow).thenReturn(true)
         `when`(view.context).thenReturn(context)
 
-        controller = QuickStatusBarHeaderController(view, quickQSPanelController)
+        controller = QuickStatusBarHeaderController(
+                view,
+                privacyIconsController,
+                statusBarIconController,
+                demoModeController,
+                quickQSPanelController,
+                qsCarrierGroupControllerBuilder,
+                qsExpansionPathInterpolator,
+                featureFlags,
+                variableDateViewControllerFactory,
+                batteryMeterViewController,
+                insetsProvider,
+                iconManagerFactory,
+        )
     }
 
     @After
