294050e5f1852b284298637cfc862a9a34c8c59b
Pranav Vashi <neobuddy89@gmail.com>
2023-05-01 10:24:01 +0530

    Revert "DragSurface: destroy surfacecontrol in view site"
    This reverts commit b7a4b6cf8ed2267d6daaac6fb6cd9553eb483a2f.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -27487,7 +27487,6 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
                 surface.destroy();
             }
             session.kill();
-            surface.destroy();
         }
     }
 
