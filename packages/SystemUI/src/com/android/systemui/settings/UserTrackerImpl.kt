Project-Kaleidoscope/android_frameworks_base

SystemUI: Fix privacy indicator not showing for apps in parallel spaces
Change-Id: Id4c301e4ab1afeb3c6eabd202b407c61472cb8c3
Signed-off-by: LibXZR <i@xzr.moe>

packages/SystemUI/src/com/android/systemui/settings/UserTrackerImpl.kt
@@ -31,6 +31,7 @@ import androidx.annotation.WorkerThread
 import com.android.systemui.Dumpable
 import com.android.systemui.dump.DumpManager
 import com.android.systemui.util.Assert
+import ink.kaleidoscope.ParallelSpaceManager;
 import java.io.FileDescriptor
 import java.io.PrintWriter
 import java.lang.IllegalStateException
@@ -111,6 +112,7 @@ class UserTrackerImpl internal constructor(
             addAction(Intent.ACTION_USER_SWITCHED)
             addAction(Intent.ACTION_MANAGED_PROFILE_AVAILABLE)
             addAction(Intent.ACTION_MANAGED_PROFILE_REMOVED)
+            addAction(Intent.ACTION_PARALLEL_SPACE_CHANGED)
         }
         context.registerReceiverForAllUsers(this, filter, null /* permission */, backgroundHandler)
 
@@ -122,7 +124,9 @@ class UserTrackerImpl internal constructor(
             Intent.ACTION_USER_SWITCHED -> {
                 handleSwitchUser(intent.getIntExtra(Intent.EXTRA_USER_HANDLE, UserHandle.USER_NULL))
             }
-            Intent.ACTION_MANAGED_PROFILE_AVAILABLE, Intent.ACTION_MANAGED_PROFILE_UNAVAILABLE -> {
+            Intent.ACTION_MANAGED_PROFILE_AVAILABLE,
+            Intent.ACTION_MANAGED_PROFILE_UNAVAILABLE,
+            Intent.ACTION_PARALLEL_SPACE_CHANGED -> {
                 handleProfilesChanged()
             }
         }
@@ -130,6 +134,7 @@ class UserTrackerImpl internal constructor(
 
     private fun setUserIdInternal(user: Int): Pair<Context, List<UserInfo>> {
         val profiles = userManager.getProfiles(user)
+        profiles.addAll(ParallelSpaceManager.getInstance().getParallelUsers())
         val handle = UserHandle(user)
         val ctx = context.createContextAsUser(handle, 0)
 
@@ -166,6 +171,7 @@ class UserTrackerImpl internal constructor(
         Assert.isNotMainThread()
 
         val profiles = userManager.getProfiles(userId)
+        profiles.addAll(ParallelSpaceManager.getInstance().getParallelUsers())
         synchronized(mutex) {
             userProfiles = profiles.map { UserInfo(it) } // save a "deep" copy
         }

imapotatook/voltage_frameworks_base

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/settings/UserTrackerImpl.kt
@@ -34,6 +34,7 @@ import androidx.annotation.WorkerThread
 import com.android.systemui.Dumpable
 import com.android.systemui.dump.DumpManager
 import com.android.systemui.util.Assert
+import ink.kaleidoscope.ParallelSpaceManager;
 import java.io.PrintWriter
 import java.lang.IllegalStateException
 import java.lang.ref.WeakReference
@@ -119,6 +120,7 @@ class UserTrackerImpl internal constructor(
             addAction(Intent.ACTION_MANAGED_PROFILE_ADDED)
             addAction(Intent.ACTION_MANAGED_PROFILE_REMOVED)
             addAction(Intent.ACTION_MANAGED_PROFILE_UNLOCKED)
+            addAction(Intent.ACTION_PARALLEL_SPACE_CHANGED)
         }
         context.registerReceiverForAllUsers(this, filter, null /* permission */, backgroundHandler)
 
@@ -134,7 +136,8 @@ class UserTrackerImpl internal constructor(
             Intent.ACTION_MANAGED_PROFILE_UNAVAILABLE,
             Intent.ACTION_MANAGED_PROFILE_ADDED,
             Intent.ACTION_MANAGED_PROFILE_REMOVED,
-            Intent.ACTION_MANAGED_PROFILE_UNLOCKED -> {
+            Intent.ACTION_MANAGED_PROFILE_UNLOCKED,
+            Intent.ACTION_PARALLEL_SPACE_CHANGED -> {
                 handleProfilesChanged()
             }
         }
@@ -148,6 +151,7 @@ class UserTrackerImpl internal constructor(
 
     private fun setUserIdInternal(user: Int): Pair<Context, List<UserInfo>> {
         val profiles = userManager.getProfiles(user)
+        profiles.addAll(ParallelSpaceManager.getInstance().getParallelUsers())
         val handle = UserHandle(user)
         val ctx = context.createContextAsUser(handle, 0)
 
@@ -218,6 +222,7 @@ class UserTrackerImpl internal constructor(
         Assert.isNotMainThread()
 
         val profiles = userManager.getProfiles(userId)
+        profiles.addAll(ParallelSpaceManager.getInstance().getParallelUsers())
         synchronized(mutex) {
             userProfiles = profiles.map { UserInfo(it) } // save a "deep" copy
         }